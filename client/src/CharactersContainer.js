734affc1be9cf2c567783f1a9f860a786b4c78ea
fresh5447 <doug@kosmojo.com>
2017-04-07 11:32:55 -0600

    char container now fetches data

--- client/src/CharactersContainer.js
+++ client/src/CharactersContainer.js
@@ -3,9 +3,19 @@ import Spinner from './Spinner';
 import CharactersList from './CharactersList';
 
 class CharactersContainer extends Component {
+
   state = {
     characters: undefined
   }
+
+  componentDidMount = () => this.fetchCharacters()
+
+  fetchCharacters = () =>
+    fetch('/api/starwars')
+      .then(blob => blob.json())
+      .then(characters => this.setState({ characters }))
+      .catch(e => e)
+
   render(){
      return (
        <div>
@@ -13,6 +23,7 @@ class CharactersContainer extends Component {
        </div>
      )
   }
+  
 }
 
 
